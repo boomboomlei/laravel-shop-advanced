3e29cfedc326ee6579436897c3a2980c03b882e0
vagrant <vagrant@homestead>
2020-08-14 06:25:54 +0000

    商品后台添加类目字段

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -7,6 +7,7 @@ use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
+use App\Models\Category;
 
 class ProductsController extends AdminController
 {
@@ -26,8 +27,12 @@ class ProductsController extends AdminController
     {
         $grid = new Grid(new Product);
 
+        $grid->model()->with(['category']);
         $grid->id('ID')->sortable();
         $grid->title('商品名称');
+
+        $grid->column('category.name','类目');
+
         $grid->on_sale('已上架')->display(function ($value) {
             return $value ? '是' : '否';
         });
@@ -59,8 +64,18 @@ class ProductsController extends AdminController
     {
         $form = new Form(new Product);
 
+        
+
+
         // 创建一个输入框，第一个参数 title 是模型的字段名，第二个参数是该字段描述
         $form->text('title', '商品名称')->rules('required');
+
+        $form->select('category_id','商品名称')->options(function($id){
+            $category=Category::find($id);
+            if($category){
+                return [$category->id=>$category->full_name];
+            }
+        })->ajax('/admin/api/categories?is_directory=0');
 
         // 创建一个选择图片的框
         $form->image('image', '封面图片')->rules('required|image');
