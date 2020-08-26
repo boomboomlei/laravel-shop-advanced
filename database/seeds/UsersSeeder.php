d9599864d1e964f06e65acc0723f0a14069486eb
vagrant <vagrant@homestead>
2020-08-26 09:15:42 +0000

    change  paswd

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -6,11 +6,11 @@ class UsersSeeder extends Seeder
 {
     public function run()
     {
-        // 通过 factory 方法生成 100 个用户并保存到数据库中
+        // 通过 factory 方法生成 100 个用户并保存到数据库中$2y$10$nLswIh3CmeO31cbzlIYvb.NeMHQhNgnWkGioCtdNPtIU2vROn906G
         factory(\App\Models\User::class, 100)->create();
 
         $lilei=\App\Models\User::query()->where(['id'=>'1'])->get();
-        $lilei->update(['email_verified_at' =>Carbon::now(),'name'=>'lilei','email'=>'lilei@qq.com','password'=>'$10$mJc9G3LblU7jsJvDhr3UbuVwGNZzAPb9b0BJwtmKk9KW38LKUUYLW']);
+        $lilei->update(['email_verified_at' =>Carbon::now(),'name'=>'lilei','email'=>'lilei@qq.com','password'=>'$2y$10$nLswIh3CmeO31cbzlIYvb.NeMHQhNgnWkGioCtdNPtIU2vROn906G']);
             
     }
 }
