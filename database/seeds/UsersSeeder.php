9e5fcd38cab50e5db798b2592baf10d6546ea9c8
vagrant <vagrant@homestead>
2020-08-26 08:44:52 +0000

    useraddemailverify

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -1,12 +1,21 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Carbon\Carbon;
 class UsersSeeder extends Seeder
 {
     public function run()
     {
         // 通过 factory 方法生成 100 个用户并保存到数据库中
-        factory(\App\Models\User::class, 100)->create();
+       $users=factory(\App\Models\User::class, 100)->create();
+
+       $emailArr=['lilei@qq.com','leilei@qq.com'];
+
+        foreach ($users as $user) {
+            if(in_array($user->email,$emailArr)){
+                $user->update(['email_verified_at' =>Carbon::now()]);
+            }
+            
+        }
     }
 }
